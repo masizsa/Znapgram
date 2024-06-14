8fdd39907a24323c4d47c4f5afda579c66af2494
zalaaaaa <ihza.nk.160903@gmail.com>
2024-06-14 22:56:25 +0700

    create [form]: create form layout and outlet for each form

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,10 @@
+import * as z from "zod";
+
+const signupValidation = z.object({
+    name: z.string().min(2, {message: "Too short"}),
+    username: z.string().min(2, {message: "Too short"}).max(50),
+    email: z.string().email(),
+    password: z.string().min(8, {message: "Password must be at least 8 character"})
+});
+
+export { signupValidation };

e89fa8b26ce8ed27642cbd71b4cb8af7ba5f3540
zalaaaaa <ihza.nk.160903@gmail.com>
2024-06-16 17:58:44 +0700

    add [validation]: add sign in validation with email and pass

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,10 +1,14 @@
 import * as z from "zod";
 
-const signupValidation = z.object({
+const SignupValidation = z.object({
     name: z.string().min(2, {message: "Too short"}),
     username: z.string().min(2, {message: "Too short"}),
     email: z.string().email(),
     password: z.string().min(8, {message: "Password must be at least 8 characters"})
 });
+const SigninValidation = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, {message: "Password must be at least 8 characters"})
+});
 
-export { signupValidation };
+export { SignupValidation, SigninValidation };

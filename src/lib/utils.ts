96e744457ed5c30bfbdb4d45d9fa5c64e53e05ec
zalaaaaa <ihza.nk.160903@gmail.com>
2024-06-19 20:13:06 +0700

    add [function]: add function to count how many times user was uploaded and function to check if user liked

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,30 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function timeAgo(dateString: string) {
+  const date = new Date(dateString);
+  const now = new Date();
+  const secondsPast = (now.getTime() - date.getTime()) / 1000;
+
+  if (secondsPast < 60) {
+      return `${Math.floor(secondsPast)} seconds ago`;
+  }
+  if (secondsPast < 3600) {
+      return `${Math.floor(secondsPast / 60)} minutes ago`;
+  }
+  if (secondsPast < 86400) {
+      return `${Math.floor(secondsPast / 3600)} hours ago`;
+  }
+  if (secondsPast < 2592000) {
+      return `${Math.floor(secondsPast / 86400)} days ago`;
+  }
+  if (secondsPast < 31536000) {
+      return `${Math.floor(secondsPast / 2592000)} months ago`;
+  }
+  return `${Math.floor(secondsPast / 31536000)} years ago`;
+}
+
+export const checkIsLiked = (likeList: string[], userId: string) => {
+    return likeList.includes(userId);
+  };

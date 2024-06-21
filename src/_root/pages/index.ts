395013b4edc81cc60292ff0b0297cd7d64d46179
zalaaaaa <ihza.nk.160903@gmail.com>
2024-06-21 20:59:22 +0700

    edit [component]: edit component in outlet for edit page

--- src/_root/pages/index.ts
+++ src/_root/pages/index.ts
@@ -5,6 +5,6 @@ export {default as Explore} from './Explore';
 export {default as PostDetails} from './PostDetails';
 export {default as Profile} from './Profile';
 export {default as Saved} from './Saved';
-export {default as UpdatePost} from './UpdatePost';
+export {default as EditPost} from './EditPost';
 export {default as UpdateProfile} from './UpdateProfile';
 export {default as LikedPost} from './LikedPost';

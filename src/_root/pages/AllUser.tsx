683ad95e1a35f066fd9a35ac3bb4c3832c986aaa
zalaaaaa <ihza.nk.160903@gmail.com>
2024-06-25 23:05:06 +0700

    create [page]: create a page to showing all top creators

--- src/_root/pages/AllUser.tsx
+++ src/_root/pages/AllUser.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Loader from '@/components/shared/Loader';
+import TopCreatorCard from '@/components/shared/TopCreatorCard';
+import { useGetAllUsers } from '@/lib/react-query/queriesAndMutations';
 
 const AllUser = () => {
+  const { data: creators, isLoading, isError } = useGetAllUsers();
+  if (isLoading) {
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    );
+  }
+
   return (
-    <div>AllUser</div>
-  )
-}
+    <div className="user-container px-10 py-12">
+      <h2 className="h3-bold">Top Creators</h2>
+      <div className="user-grid">
+        {creators?.map((creator)=>(
+            <TopCreatorCard creator={creator} key={creator.$id}/>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default AllUser
